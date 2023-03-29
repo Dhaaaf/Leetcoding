25b036f31e1ff0dd34fbaf28e9732214eed5a926
Fahd Ahsan <sfahsan@umich.edu>
2023-03-29 15:43:13 -0400

    Zipper done

---
+++ structy/linkedList/6.zipperLists.py
@@ -0,0 +1,61 @@
+# Write a function, zipper_lists, that takes in the head of two linked lists as arguments. The function should zipper the two lists together into single linked list by alternating nodes. If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. The function should return the head of the zippered linked list.
+
+# Do this in-place, by mutating the original Nodes.
+
+# You may assume that both input lists are non-empty.
+
+# My iterative solution:
+
+def zipper_lists(head_1, head_2):
+  one = head_1
+  two = head_2
+  
+  while one.next and two.next:
+    next_one = one.next
+    next_two = two.next
+    one.next, two.next = two, next_one
+    one = next_one
+    two = next_two
+  if one and two:
+    next = one.next
+    one.next = two
+  if two and next:
+    two.next = next
+  return head_1
+
+# Alvins iterative solution:
+def zipper_lists(head_1, head_2):
+  tail = head_1
+  current_1 = head_1.next
+  current_2 = head_2
+  count = 0
+  while current_1 is not None and current_2 is not None:
+    if count % 2 == 0:
+      tail.next = current_2
+      current_2 = current_2.next
+    else:
+      tail.next = current_1
+      current_1 = current_1.next
+    tail = tail.next
+    count += 1
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2
+    
+  return head_1
+
+# Alvin's recursive solution:
+def zipper_lists(head_1, head_2):
+  if head_1 is None and head_2 is None:
+    return None
+  if head_1 is None:
+    return head_2
+  if head_2 is None:
+    return head_1
+  next_1 = head_1.next
+  next_2 = head_2.next
+  head_1.next = head_2
+  head_2.next = zipper_lists(next_1, next_2)
+  return head_1  
