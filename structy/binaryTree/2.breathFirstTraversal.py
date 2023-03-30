2fc615eb92d5064191cfb0f7e195cb983d7ca55f
Fahd Ahsan <sfahsan@umich.edu>
2023-03-30 16:28:15 -0400

    breath first done

---
+++ structy/binaryTree/2.breathFirstTraversal.py
@@ -0,0 +1,43 @@
+# Write a function, breadth_first_values, that takes in the root of a binary tree. The function should return a list containing all values of the tree in breadth-first order.d
+
+
+# My iterative solution:
+
+def breadth_first_values(root):
+  if not root:
+    return []
+  
+  queue = [root]
+  values = []
+  while queue:
+    node = queue.pop(0)
+    values.append(node.val)
+    if node.left:
+      queue.append(node.left)
+    if node.right:
+      queue.append(node.right)
+  return values
+
+
+#Alvin's iterative solution:
+from collections import deque
+
+def breadth_first_values(root):
+  if not root:
+    return []
+  
+  queue = deque([ root ])
+  values = []
+  
+  while queue:
+    node = queue.popleft()
+    
+    values.append(node.val)
+    
+    if node.left:
+      queue.append(node.left)
+      
+    if node.right:
+      queue.append(node.right)
+      
+  return values
