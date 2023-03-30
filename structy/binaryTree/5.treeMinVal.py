d315595f5e0ce4c8c49d2a3c38bf781a7bdc5beb
Fahd Ahsan <sfahsan@umich.edu>
2023-03-30 16:50:10 -0400

    5 done

--- structy/binaryTree/5.treeMinVal.py
+++ structy/binaryTree/5.treeMinVal.py
@@ -1,5 +1,9 @@
+# Write a function, tree_min_value, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
+
+# You may assume that the input tree is non-empty.
 
 
+# My breath first solution:
 
 
 from collections import deque
