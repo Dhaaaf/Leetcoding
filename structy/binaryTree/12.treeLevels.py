61d6458c7744ee043aea05350dd9a8fc4bb29a33
Fahd Ahsan <sfahsan@umich.edu>
2023-03-31 17:59:34 -0400

    12 done properly

--- structy/binaryTree/12.treeLevels.py
+++ structy/binaryTree/12.treeLevels.py
@@ -12,3 +12,16 @@ def tree_levels(root):
   
   while queue:
     current, level_num = queue.popleft()
+    
+    if len(levels) == level_num:
+      levels.append([current.val])
+    else:
+      levels[level_num].append(current.val)
+      
+    if current.left is not None:
+      queue.append((current.left, level_num +1))
+    
+    if current.right is not None:
+      queue.append((current.right, level_num +1))
+      
+  return levels
