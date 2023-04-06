ef38852467f83a83360b243e9ed7c68300227c32
Fahd Ahsan <sfahsan@umich.edu>
2023-04-06 19:09:17 -0400

    fib done iteratively with memo

--- structy/dynamicProgramming/1.fib.py
+++ structy/dynamicProgramming/1.fib.py
@@ -9,6 +9,23 @@
 # Solve this recursively.
 
 
+# My iterative memoization solution:
+
+def fib(n):
+  memo = {0: 0, 1: 1}
+  
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  
+  
+  for i in range(2, n + 1):
+    memo[i] = memo[i-1] + memo[i-2]
+  
+  return memo[n]
+
+
 
 # Memoization solution:
 def fib(n):
