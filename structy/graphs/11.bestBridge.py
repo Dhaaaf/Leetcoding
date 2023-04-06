9ad08fd4d8e63fd610eb80eec6656314344cfa6f
Fahd Ahsan <sfahsan@umich.edu>
2023-04-06 14:16:36 -0400

    Done with 11

---
+++ structy/graphs/11.bestBridge.py
@@ -0,0 +1,112 @@
+# Write a function, best_bridge, that takes in a grid as an argument. The grid contains water (W) and land (L). There are exactly two islands in the grid. An island is a vertically or horizontally connected region of land. Return the minimum length bridge needed to connect the two islands. A bridge does not need to form a straight line.
+
+
+
+
+# My best bridge iterative solutions:
+from collections import deque
+
+
+def best_bridge(grid):
+  main_island = None
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      potential_island = traverse_island(grid, r, c)
+      if len(potential_island) > 0:
+        main_island = potential_island
+  
+  visited = set(main_island)
+  queue = deque([])
+  for pos in main_island:
+    r, c = pos
+    queue.append((r, c, 0))
+    
+  while queue:
+    row, col, distance = queue.popleft()
+    if grid[row][col] == 'L' and (row, col) not in main_island:
+      return distance - 1
+    
+    for neighbor_row, neighbor_col in [(row + dr, col + dc) for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]]:
+      neighbor_pos = (neighbor_row, neighbor_col)
+      if inbounds(grid, neighbor_row, neighbor_col) and neighbor_pos not in visited:
+          visited.add(neighbor_pos)
+          queue.append((neighbor_row, neighbor_col, distance + 1))
+  
+
+def inbounds(grid, row, col):
+  row_valid = 0 <= row < len(grid)
+  col_valid = 0 <= col < len(grid[0])
+  return row_valid and col_valid
+
+
+def traverse_island(grid, row, col):
+    visited = set()
+    pos = (row, col)
+    stack = [(row, col)]
+    while stack:
+        pos = stack.pop()
+        r, c = pos
+
+        if inbounds(grid, r, c) and grid[r][c] == "L" and pos not in visited:
+            visited.add(pos)
+            # stack.append((row + 1, col))
+            # stack.append((row - 1, col))
+            # stack.append((row, col + 1))
+            # stack.append((row, col - 1))
+            stack.extend([(r + dr, c + dc) for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]])
+
+    return visited 
+
+
+# Alvin's recursive solution:
+from collections import deque
+
+def best_bridge(grid):
+  main_island = None
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      potential_island = traverse_island(grid, r, c, set())
+      if len(potential_island) > 0:
+        main_island = potential_island
+  
+  visited = set(main_island)
+  queue = deque([ ])
+  for pos in main_island:
+    r, c = pos
+    queue.append((r, c, 0))
+  
+  while queue:
+    row, col, distance = queue.popleft()
+    if grid[row][col] == 'L' and (row, col) not in main_island:    
+      return distance - 1
+    
+    deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    for delta in deltas:
+      delta_row, delta_col = delta
+      neighbor_row = row + delta_row
+      neighbor_col = col + delta_col
+      neighbor_pos = (neighbor_row, neighbor_col)
+      if inbounds(grid, neighbor_row, neighbor_col) and neighbor_pos not in visited:
+        visited.add(neighbor_pos)
+        queue.append((neighbor_row, neighbor_col, distance + 1))
+  
+def inbounds(grid, row, col):
+  row_inbounds = 0 <= row < len(grid)
+  col_inbounds = 0 <= col < len(grid[0])
+  return row_inbounds and col_inbounds
+  
+def traverse_island(grid, row, col, visited):
+  if not inbounds(grid, row, col) or grid[row][col] == 'W':
+    return visited
+  
+  pos = (row, col)
+  if pos in visited:
+    return visited
+  
+  visited.add(pos)
+  
+  traverse_island(grid, row - 1, col, visited)
+  traverse_island(grid, row + 1, col, visited)
+  traverse_island(grid, row, col - 1, visited)
+  traverse_island(grid, row, col + 1, visited)
+  return visited
