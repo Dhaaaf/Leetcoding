3bcfec2507b9ba957f8ed330d48d0867fd949c88
Fahd Ahsan <sfahsan@umich.edu>
2023-04-07 14:49:23 -0400

    tribonacci

---
+++ structy/dynamicProgramming/2.tribonacci.py
@@ -0,0 +1,28 @@
+# Write a function tribonacci that takes in a number argument, n, and returns the n-th number of the Tribonacci sequence.
+
+# The 0-th and 1-st numbers of the sequence are both 0.
+
+# The 2-nd number of the sequence is 1.
+
+# To generate further numbers of the sequence, calculate the sum of previous three numbers.
+
+# Solve this recursively.
+
+# Mine and Alvin's solution:
+def tribonacci(n):
+  memo = {}
+  return _trib(n, memo)
+
+def _trib(n, memo):
+  if n == 0 or n == 1:
+    return 0
+  
+  if n == 2:
+    return 1
+  
+  if n in memo:
+    return memo[n]
+  
+  memo[n] = _trib(n -1 , memo) + _trib(n - 2, memo) + _trib(n - 3, memo)
+  
+  return memo[n]
