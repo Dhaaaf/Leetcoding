acdf51b63d2b8252cccf6cdcaa86741987d9874d
Fahd Ahsan <sfahsan@umich.edu>
2023-04-11 14:03:18 -0400

    fixed 222

--- 222.countCompleteTreeNodes.py
+++ 222.countCompleteTreeNodes.py
@@ -11,3 +11,36 @@ class Solution:
         if root.right == None and root.left == None:
             return 1
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+    
+
+# Correct solution:
+
+def countNodes(root: TreeNode) -> int:
+    if not root:
+        return 0
+
+    # Calculate the depth of the tree
+    depth = 0
+    node = root
+    while node.left:
+        depth += 1
+        node = node.left
+
+    # Binary search to find the number of nodes in the last level
+    left, right = 0, 2 ** depth - 1
+    node = root
+    while left < right and node:
+        mid = (left + right) // 2
+        if (1 << (depth - 1)) & mid == 0:
+            node = node.left
+        else:
+            node = node.right
+        depth -= 1
+
+        if not node:
+            right = mid
+        else:
+            left = mid + 1
+
+    # Total number of nodes = nodes in all levels except last + nodes in the last level
+    return (2 ** depth - 1) + left
