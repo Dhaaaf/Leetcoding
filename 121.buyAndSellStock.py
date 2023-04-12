fa9028f5284e4228e6d124611f8e0383fc8b3b6a
Fahd Ahsan <sfahsan@umich.edu>
2023-04-12 17:56:24 -0400

    Done with buy and sell stock

--- 121.buyAndSellStock.py
+++ 121.buyAndSellStock.py
@@ -3,3 +3,14 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            profit = max(profit, price - lowest)
+        return profit
