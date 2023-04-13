1c52b5d0b098861d4670240619986e129165c1a9
Fahd Ahsan <sfahsan@umich.edu>
2023-04-13 16:52:29 -0400

    done with permutations

---
+++ structy/exhaustiveRecursion/2.permutations.py
@@ -0,0 +1,19 @@
+# Write a function, permutations, that takes in a list an argument. The function should return a 2D list where each sublist represents one of the possible permutations of the list.
+
+# The sublists may be returned in any order.
+
+# You may assume that the input list contains unique items.
+
+
+def permutations(items):
+  if not items:
+    return [[]]
+  
+  item = items.pop()
+  perms = permutations(items)
+  result = []
+  for perm in perms:
+    for i in range(len(perm) + 1):
+      result.append([*perm[:i], item, *perm[i:]])
+  
+  return result
