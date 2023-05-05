b37eb51221299c8552e3dabd8927ad6a899ce1c7
Fahd Ahsan <sfahsan@umich.edu>
2023-05-05 15:40:40 -0400

    post-order traversal done

---
+++ structy/mixedrecall/18.postOrderTraversal.py
@@ -0,0 +1,30 @@
+# Write a function, post_order, that takes in the root of a binary tree. The function should return a list containing the post-ordered values of the tree.
+
+# Post-order traversal is when nodes are recursively visited in the order: left child, right child, self.
+
+
+# My Iterative Solution:
+def post_order(root):
+    if root is None:
+        return []
+    
+    result = []
+    stack = [(root, False)]
+    
+    while stack:
+        node, visited = stack.pop()
+        
+        if node is None:
+            continue
+        
+        if visited:
+            result.append(node.val)
+        else:
+            # Push the node back into the stack with the visited flag set to True
+            stack.append((node, True))
+            
+            # Push the right and left children into the stack
+            stack.append((node.right, False))
+            stack.append((node.left, False))
+            
+    return result
