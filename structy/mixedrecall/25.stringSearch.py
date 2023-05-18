0b35ba710a50a597c6a69c5a3586ca72700aa36a
Fahd Ahsan <sfahsan@umich.edu>
2023-05-18 17:26:01 -0400

    done with string search recursively

--- structy/mixedrecall/25.stringSearch.py
+++ structy/mixedrecall/25.stringSearch.py
@@ -1,4 +1,6 @@
+# Write a function, string_search, that takes in a grid of letters and a string as arguments. The function should return a boolean indicating whether or not the string can be found in the grid as a path by connecting horizontal or vertical positions. The path can begin at any position, but you cannot reuse a position more than once in the path.
 
+# You can assume that all letters are lowercase and alphabetic.
 
 
 
@@ -21,3 +23,35 @@ def string_search(grid, s):
                             new_visited.add((new_r, new_c))
                             stack.append((new_r, new_c, curr_s[1:], new_visited))
     return False
+
+
+
+# Alvin's recursive string search solution:
+def string_search(grid, s):
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):    
+      if dfs(grid, r, c, s):
+        return True
+  return False
+
+
+def dfs(grid, r, c, s):
+  if s == '':
+    return True
+  
+  row_inbounds = 0 <= r < len(grid)
+  col_inbounds = 0 <= c < len(grid)
+  if not row_inbounds or not col_inbounds:
+    return False
+  
+  char = grid[r][c]
+  if char != s[0]:
+    return False
+  
+  suffix = s[1:]
+  
+  grid[r][c] = '*'
+  
+  result = dfs(grid, r + 1, c, suffix) or dfs(grid, r - 1, c, suffix) or dfs(grid, r, c + 1, suffix) or dfs(grid, r, c - 1, suffix)
+  grid[r][c] = char
+  return result
