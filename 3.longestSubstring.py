ba425fd7f73979b24b9434ac2eaf05c7884d4211
Fahd Ahsan <sfahsan@umich.edu>
2023-06-07 13:27:32 -0400

    new longest substring solution

--- 3.longestSubstring.py
+++ 3.longestSubstring.py
@@ -17,3 +17,18 @@ class Solution:
                 i += 1
 
         return max_length
+    
+
+# Neet code solution:class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
