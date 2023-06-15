55e824d6aa346d8fc693545fffae297eef6c44d8
Fahd Ahsan <sfahsan@umich.edu>
2023-06-15 16:22:10 -0400

    added 567 with dictionary instead of counter

--- 567.permutationInString.py
+++ 567.permutationInString.py
@@ -5,7 +5,7 @@
 
 
 
-
+# With Counter
 from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
@@ -28,3 +28,35 @@ class Solution:
             if s1_counter == window_counter:
                 return True
         return False
+    
+
+
+# With Dictionaries
+def checkInclusion(s1: str, s2: str) -> bool:
+    s1_len, s2_len = len(s1), len(s2)
+    if s1_len > s2_len:
+        return False
+
+    # Create dictionaries
+    s1_dict = {ch: 0 for ch in s1}
+    window_dict = {ch: 0 for ch in s1}
+
+    for ch in s1:
+        s1_dict[ch] += 1
+
+    for i in range(s2_len):
+        if s2[i] in window_dict:
+            window_dict[s2[i]] += 1
+
+        # Remove one character from the left side of the window
+        if i >= s1_len and s2[i - s1_len] in window_dict:
+            if window_dict[s2[i - s1_len]] == 1:
+                del window_dict[s2[i - s1_len]]
+            else:
+                window_dict[s2[i - s1_len]] -= 1
+
+        # Compare the frequency of characters in the current window with s1
+        if s1_dict == window_dict:
+            return True
+
+    return False
